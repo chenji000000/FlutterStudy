70ba5f08079014285cfb53838279c01a4f7e8d93
chenji <chenji000000@163.com>
2019-07-05 22:11:00 +0800

    add examples

---
+++ examples/using_tabs/lib/main.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:using_tabs/tabs/first.dart';
+import 'package:using_tabs/tabs/Second.dart';
+import 'package:using_tabs/tabs/Third.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'using tabs'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+  TabController controller;
+  
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller = TabController(length: 3, vsync: this);
+  }
+  
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    controller.dispose();
+    super.dispose();
+  }
+  
+  TabBar getTabBar() {
+    return TabBar(
+      tabs: <Widget>[
+        Tab(
+          icon: Icon(Icons.favorite)
+        ),
+        Tab(
+            icon: Icon(Icons.adb)
+        ),
+        Tab(
+            icon: Icon(Icons.airport_shuttle)
+        )
+      ],
+      controller: controller,
+    );
+  }
+  
+  TabBarView getTabBarView(var tabs) {
+    return TabBarView(
+      children: tabs,
+      controller: controller,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+        backgroundColor: Colors.blue,
+        bottom: getTabBar(),
+      ),
+      body: getTabBarView(<Widget>[First(), Second(), Third()])
+    );
+  }
+}
